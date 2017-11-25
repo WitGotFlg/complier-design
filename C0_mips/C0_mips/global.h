f0e9534213cbd14a0cd964d20187201ccc8eafe6
摘星者\msi-pc <1317398662@qq.com>
2017-11-25 08:58:07 +0800

    词法分析程序初步完成，能做到要求的输出，但是是在控制台，需要加上输出到文件

---
+++ C0_mips/C0_mips/global.h
@@ -0,0 +1,13 @@
+#ifndef GLOBAL_H_INCLUDED
+#define GLOBAL_H_INCLUDED
+#include <iostream>
+#include <fstream>
+#include <cassert>
+#include <string>
+#include<map>
+#include <stdlib.h>
+#include <string.h>
+
+void readCodeFile();
+
+#endif // GLOBAL_H_INCLUDED
---
+++ C0_mips/C0_mips/main.cpp
@@ -0,0 +1,12 @@
+
+#include "global.h"
+
+
+using namespace std;
+
+int main()
+{
+    //cout << "Hello world!" << endl;
+    readCodeFile();
+    return 0;
+}
